277ee35f89112035a0c70443afdb2523df12694c
LeoMbm <leonidas.j.mbm@gmail.com>
2022-07-20 17:03:54 +0200

    vide

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from xml.etree.ElementInclude import include
+from django.urls import path
+
+# from users.views import create_user
+from django.urls import path, include
+
+urlpatterns = [
+
+    path('', include('dj_rest_auth.urls')),
+    path('register/', include('dj_rest_auth.registration.urls'))
+]
