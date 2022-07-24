2da97b1984eae9ecdfe98c55e5c6eb7ea9888673
LeoMbm <leonidas.j.mbm@gmail.com>
2022-07-25 01:19:11 +0200

    resolv conflicts

--- trade/urls.py
+++ trade/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from trade.views import Trade
-tr = Trade(requests)
+from trade.views import trade_open, get_list_cryptocurrency, get_realtime_price, get_pricemarket
+
 urlpatterns = [
-    path('open/', tr.trade_open, name='open-trade'),
+    path('open/', trade_open, name='open-trade'),
     path('<str:symbol_name>/marketprice/', get_pricemarket, name='get_pricemarket'),
     path('all/cryptocurrency/', get_list_cryptocurrency, name='get_pricemarket'),
     path('<str:symbol>/price/', get_realtime_price, name='get_realtime_price'),
--- trade/views.py
+++ trade/views.py
@@ -19,34 +19,34 @@ td = TDClient(apikey=key)
 
 
 # TODO: Read about viewset
-class Trade(APIView):
-    @api_view(['POST'])
-    def trade_open(request):
-        serializer = TradeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
 
-    @api_view(['GET'])
-    def get_pricemarket(request, symbol_name):
-        # Construct the necessary time series
-        ts = td.time_series(
-            symbol=symbol_name,
-            interval="1min",
-            outputsize=20,
-        )
-        return Response(ts.as_json())
+@api_view(['POST'])
+def trade_open(request):
+    serializer = TradeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
 
-    @api_view(['GET'])
-    def get_realtime_price(request, symbol):
-        url = f'https://api.twelvedata.com/price?symbol={symbol}&apikey={key}'
-        res = requests.get(url).json()
+@api_view(['GET'])
+def get_pricemarket(request, symbol_name):
+    # Construct the necessary time series
+    ts = td.time_series(
+        symbol=symbol_name,
+        interval="1min",
+        outputsize=20,
+    )
+    return Response(ts.as_json())
 
-        return Response(res)
+@api_view(['GET'])
+def get_realtime_price(request, symbol):
+    url = f'https://api.twelvedata.com/price?symbol={symbol}&apikey={key}'
+    res = requests.get(url).json()
 
-    @api_view(['GET'])
-    def get_list_cryptocurrency(request):
-        url = 'https://api.twelvedata.com/cryptocurrencies'
-        res = requests.get(url).json()
+    return Response(res)
 
-        return Response(res)
+@api_view(['GET'])
+def get_list_cryptocurrency(request):
+    url = 'https://api.twelvedata.com/cryptocurrencies'
+    res = requests.get(url).json()
+
+    return Response(res)
