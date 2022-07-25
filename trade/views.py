907c857cfb59b58a14a03bf9d7d9c656e380c001
ozcanemre <ozcan.emre1608@gmail.com>
2022-07-25 15:18:33 +0200

    wire get and post

--- trade/views.py
+++ trade/views.py
@@ -35,13 +35,16 @@ def create_wire(request):
     trade_price = Trade.objects.filter(profile_id=request.user).aggregate(balance=Coalesce(Sum((F('close_price') - F('open_price'))*F('quantity')), 0))
     balance=dict(Counter(wire_total)+Counter(trade_price))
     serializer = WireSerializer(data=request.data)
+    amount=serializer.initial_data['amount']
+    withdraw=serializer.initial_data['withdraw']
     
-    # if balance['balance']<0:
-    #     data = {"message": "not enough money",}
-    #     return Response(data)
-    # else:
+    
+    if wire_total['balance']+trade_price['balance']-amount<0 and withdraw==True:
+        data = {"message": "not enough money",}
+        return Response(data)
+    else:
 
-    if serializer.is_valid():
+      if serializer.is_valid():
         
            serializer.save(user_id=request.user)
            return Response(serializer.data)
@@ -67,13 +70,13 @@ def index_balance(request):
     wire_total = Wire.objects.filter(user_id=request.user).aggregate(balance=Coalesce(Sum('amount'),0))
     trade_price = Trade.objects.filter(profile_id=request.user).aggregate(balance=Coalesce(Sum((F('close_price') - F('open_price'))*F('quantity')), 0))
     profile=Profile.objects.filter(id=request.user.id).values('id','username','email','first_name','last_name','adress').first()
+
     if wire_total['balance'] ==0 and trade_price['balance'] ==0:
         profile.update(default_balance)
         return Response(profile)
     else:
         balance = dict(Counter(wire_total) + Counter(trade_price))
         profile.update(balance)
-        print(wire_total['balance'])
         return Response(profile)
 
 
